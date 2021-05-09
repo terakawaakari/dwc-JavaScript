06d22de67c02f8c3bcff46919898506c5ced9c57
terakawaakari <terakawaakari0701@gmail.com>
2021-05-09 23:46:52 +0900

    [Finish]JS12

---
+++ Ajax/JS12.js
@@ -0,0 +1,21 @@
+API_KEY = "95efc750dbfa50235935372a31ee4ccc"
+
+$(function(){
+  $('#btn').on('click',function(){
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + "95efc750dbfa50235935372a31ee4ccc",
+      dataType : 'jsonp',
+    }).done(function (data){
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+    }).fail(function (data){
+      alert('通信に失敗しました。')
+    });
+  });
+});
